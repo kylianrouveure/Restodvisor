ff03e19c774bfbc0c7077e3976e74eb6fc3b406b
Luckultur <115996893+sfvdqfgsawvbn@users.noreply.github.com>
2023-11-08 13:34:37 +0100

    Page Restaurateur HTML V2

---
+++ userv2.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Carte des Restaurants de Lyon</title>
+<style>
+  body {
+    font-family: 'Limelight', sans-serif;
+    background-color: #003400;
+    color: #ffcc00;
+    margin: 0;
+    padding: 0;
+  }
+
+  .header {
+    background-color: #fbebce;
+    color: #003400;
+    padding: 20px;
+    text-align: center;
+  }
+
+  .header h1 {
+    font-family: 'Playfair Display', serif;
+  }
+
+  .restaurant-map {
+    width: 100%;
+    height: 500px; /* Example height */
+    background-color: #fbebce;
+    margin-top: 20px;
+  }
+
+  .restaurant-details {
+    display: none;
+    background-color: #fbebce;
+    padding: 20px;
+  }
+
+  .restaurant-details.visible {
+    display: block;
+  }
+
+  /* Additional styles can be added here */
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>Restaurants de Lyon</h1>
+</div>
+
+<div id="map" class="restaurant-map">
+  <!-- Ici, vous pouvez intégrer une carte interactive avec les restaurants -->
+</div>
+
+<div class="restaurant-details" id="details">
+  <h2>Nom du Restaurant</h2>
+  <img src="image_du_restaurant.jpg" alt="Photo du Restaurant">
+  <p>Menu et prix moyens ici</p>
+  <p>Avis et notes ici</p>
+  <!-- Plus de détails ici -->
+</div>
+
+<script>
+  // Ici, vous pouvez ajouter du JavaScript pour gérer les interactions
+</script>
+
+</body>
+</html>
